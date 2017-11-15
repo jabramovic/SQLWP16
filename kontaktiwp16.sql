b679a639408b6b5a987c902cb9549f9bace12907
jabramovic <jabramovic95@gmail.com>
2017-11-15 18:54:50 +0100

    promjena sef

--- kontaktiwp16.sql
+++ kontaktiwp16.sql
@@ -4,18 +4,19 @@ create database kontaktiwp16;
 use kontaktiwp16;
 
 create table osoba(
-sifra int not null primary key auto_increment, 
+sifra int not null primary key auto_increment,
 ime varchar(50) not null,
 spol boolean,
 datumrodenja datetime,
-nadimak varchar(20)
-
+nadimak varchar(20),
+sef int not null
 );
 
 create table kontakt(
-sifra int not null primary key auto_increment, 
+sifra int not null primary key auto_increment,
 osoba int not null,
 telefon varchar(20)
 );
 
 alter table kontakt add foreign key (osoba) references osoba(sifra);
+alter table osoba add foreign key (sef) references osoba(sifra);
